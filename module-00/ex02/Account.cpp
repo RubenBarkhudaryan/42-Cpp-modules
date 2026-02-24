f6f62f4b1ada20e831c053a4c295d316baf600fb
RubBarkhudaryan <rubenbarkhudaryan2005@gmail.com>
2026-02-24 19:46:41 +0400

    dtor line added

--- module-00/ex02/Account.cpp
+++ module-00/ex02/Account.cpp
@@ -32,6 +32,7 @@ Account::Account(int initial_deposit)
 
 Account::~Account(void)
 {
+	--(Account::_nbAccounts);
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex
 			<< ";amount:" << this->_amount
