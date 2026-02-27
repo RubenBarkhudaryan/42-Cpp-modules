aa3572cc2e86afad703295a1ec22a48ff9a0ccc4
RubBarkhudaryan <rubenbarkhudaryan2005@gmail.com>
2026-02-27 16:28:18 +0400

    module-01 -> ex05 (function pointers usage as an array of function pointers)

---
+++ module-01/ex05/Harl.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "./Harl.hpp"
+
+void	Harl::debug(void)
+{
+	std::cout << CYAN << DEBUG << RESET << std::endl;
+}
+void	Harl::info(void)
+{
+	std::cout << GREEN << INFO << RESET << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << YELLOW << WARNING << RESET << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << RED << ERROR << RESET << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	void	(Harl::*functions[4])(void) = 
+	{
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error,
+	};
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (levels[i] == level)
+		{
+			(this->*functions[i])();
+			break ;
+		}
+	}
+}
---
+++ module-01/ex05/Harl.hpp
@@ -0,0 +1,30 @@
+#ifndef	HARL_CPP
+
+# define HARL_CPP
+
+# define DEBUG		"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+# define INFO		"I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+# define WARNING	"I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month."
+# define ERROR		"This is unacceptable! I want to speak to the manager now."
+
+# define RESET		"\033[0m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define CYAN		"\033[36m"
+
+# include <string>
+
+class	Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		void	complain(std::string level);
+};
+
+#endif //HARL_CPP
